978c2b836364e48a21a5810b550e3c7d45a7395f
blazerrt86899 <tromu98@gmail.com>
2020-04-28 21:56:50 +0530

    Customized Spring boot App for Book Store

--- onlinebookstore/src/main/java/com/onlinebookstore/onlinebookstore/entity/Book.java
+++ onlinebookstore/src/main/java/com/onlinebookstore/onlinebookstore/entity/Book.java
@@ -1,25 +1,62 @@
 package com.onlinebookstore.onlinebookstore.entity;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
 import java.sql.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "tbl_book")
-public class Book {
+@Setter
+@Getter
+@ToString
+public class Book implements Serializable{
 	
-	private Integer bookId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	private String sku;
+	
+	@Column(name = "name")
 	private String bookName;
+	
 	private String description;
-	private Double unitPrice;
+	
+	@Column(name = "unit_price")
+	private BigDecimal unitPrice;
+	
+	@Column(name = "image_url")
 	private String imageURL;
+	
 	private Boolean active;
+	
+	@Column(name = "units_in_stock")
 	private Integer stockNumber;
+	
+	@Column(name = "date_created")
 	private Date dateCreated;
+	
+	@Column(name = "last_updated")
 	private Date lastUpdated;
-	private BookCategory categoryId;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id",nullable = false)
+	//@OnDelete(action = OnDeleteAction.CASCADE)
+	private BookCategory category;
 	
 	
 
--- onlinebookstore/src/main/java/com/onlinebookstore/onlinebookstore/entity/BookCategory.java
+++ onlinebookstore/src/main/java/com/onlinebookstore/onlinebookstore/entity/BookCategory.java
@@ -1,13 +1,35 @@
 package com.onlinebookstore.onlinebookstore.entity;
 
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
-@Table(name = "tbl_book_category")
-public class BookCategory {
+@Table(name = "tbl_category")
+@Setter
+@Getter
+@ToString
+public class BookCategory implements Serializable	{
 	
-	private Integer categoryId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "category_name")
 	private String categoryName;
 
+	@OneToMany(cascade = CascadeType.ALL,mappedBy = "category")
+	private Set<Book> book;
 }
